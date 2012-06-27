2c9328a571bf853b6879a5975885c3f2821e0172
Sam Serpoosh <ssjesus287@gmail.com>
2012-06-27 15:38:30 +0430

    when users try to go to the page for a non-existance project they should be redirected back to index page with appropriate alert message

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project_by_passed_id, :only => [:show, :edit, 
+                                                      :update, :destroy]
 
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -24,11 +25,9 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -39,10 +38,20 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
+
+  private
+
+    def find_project_by_passed_id
+      begin
+        @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = "Project you were looking for could not be found"
+        redirect_to projects_path
+      end
+    end
 
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe ProjectsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "accessing a no-longer existed project" do
+    it "redirects to index page and show and alert message" do
+      get :show, :id => "not-existed"
+      flash[:alert].should =~ /could not be found/
+      response.should redirect_to(projects_path)
     end
   end
 
