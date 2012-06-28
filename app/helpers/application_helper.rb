3a4cb19369c9d2ab14b87417a25fd51d1383a95d
Sam Serpoosh <ssjesus287@gmail.com>
2012-06-28 15:03:03 +0430

    refatored the application helper and application layout view for removing the logic in the view code and moved it to helper (kind of a presenter pattern conformation)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,16 @@
 module ApplicationHelper
 
   def title(*parts)
-    unless parts.empty?
-      content_for :title do
-        (parts << "Ticketee").join(" - ")
-      end
+    @main_title = "Ticketee"
+    if parts.empty? == false
+      @main_title = (parts << @main_title).join(" - ")
     end
+
+    @main_title
   end
   
   def main_title
-    "Ticketee"
+    @main_title ||= "Ticketee"
   end
 
 end
