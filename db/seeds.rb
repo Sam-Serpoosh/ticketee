f083feb3c030a2b84ab18d7a6f96cca72c70d133
Sam Serpoosh <ssjesus287@gmail.com>
2012-07-08 18:55:15 +0430

    added a seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.create!(:email => "admin@ticketee.com",
+                          :password => "password")
+
+admin_user.admin = true
+admin_user.confirm!
+admin_user.save!
+
+Project.create!(:name => "Ticketee Beta")
